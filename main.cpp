82a6ed3c11039ceff9c0edeb2e9ada14ba30b41a
Charlotte <29734551+CharlotteCross1998@users.noreply.github.com>
2024-07-14 18:39:41 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+#include <random>
+int main(int argc, char** argv)
+{
+	std::random_device r;
+    std::default_random_engine rand(r());
+    std::uniform_int_distribution<int> uniform_dist(0, 100);
+    if(argc == uniform_dist(rand))
+    {
+        std::cout << "Congratulations, you've won!\n";
+    }
+    else 
+    {
+        std::cout << "Error: Incorrect number of arguments passed\n";
+    }
+    return EXIT_SUCCESS;
+}
